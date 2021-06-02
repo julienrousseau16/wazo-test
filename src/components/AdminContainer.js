edc3e882f8d190cc4a7498553b6de7d9c60026ce
Julien Rousseau <julienrousseau16@gmail.com>
2021-06-02 21:14:56 +0200

    new user feature is functional

--- src/components/AdminContainer.js
+++ src/components/AdminContainer.js
@@ -3,17 +3,13 @@ import { Redirect } from 'react-router-dom'
 import { TokenContext } from '../tokenContext'
 import axios from 'axios'
 
-// import { Admin, Resource } from 'react-admin'
-// import restProvider from 'ra-data-simple-rest'
-
-// import UsersList from './UsersList'
-
 import CreateUser from './CreateUser'
 import User from './User'
 
 const AdminContainer = () => {
 
   const { token } = useContext(TokenContext)
+  const usersUrl = 'https://quintana.wazo.community/api/auth/0.1/users'
   const auth = {
     headers: {
       'X-Auth-Token': token
@@ -21,39 +17,32 @@ const AdminContainer = () => {
   }
   const [usersList, setUsersList] = useState([])
 
-  const fetchData = () => {
-    const url = 'https://quintana.wazo.community/api/auth/0.1/users'
+  const fetchUsers = () => {
     axios
-      .get(url, auth)
+      .get(usersUrl, auth)
       .then(res => {
-        // console.log(res.data.items[0])
         setUsersList(res.data.items)
+        console.log('repeat ?')
       })
   }
 
-  // const dataProvider = () => {
-  //   axios.getList('https://quintana.wazo.community/api/auth/0.1/users', auth)
-  //     .then(res => console.log(res.data))
-  // }
-
   useEffect(() => {
-    fetchData()
-  })
+    fetchUsers()
+  }, [])
 
   if (token !== '') {
 
     return (
       <div>
         <h2>List of Users</h2>
-        {/* <Admin dataProvider={dataProvider}>
-          <Resource name='users' list={UsersList} />
-        </Admin>
-        <button onClick={fetchData}>FETCH</button> */}
-
         {
           usersList.map(user => <User key={user.uuid} user={user} />)
         }
-        <CreateUser token={token} />
+        <CreateUser
+          auth={auth}
+          usersUrl={usersUrl}
+          usersList={usersList}
+          setUsersList={setUsersList} />
       </div>
     )
   } else {
--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -1,16 +1,19 @@
 import React, { useState } from 'react'
+import axios from 'axios'
 
-const CreateUser = ({ token }) => {
+const CreateUser = ({ auth, usersUrl, usersList, setUsersList }) => {
 
   const initialValues = {
-    username: '',
+    email_address: '',
+    enabled: true,
     firstname: '',
     lastname: '',
-    email: '',
-    purpose: ''
+    password: '',
+    purpose: '',
+    username: ''
   }
   const [formData, setFormData] = useState(initialValues)
-  const { username, firstname, lastname, email, purpose } = formData
+  const { email_address, firstname, lastname, password, purpose, username } = formData
 
   const handleChange = e => {
     const key = e.target.name
@@ -18,23 +21,32 @@ const CreateUser = ({ token }) => {
     setFormData(prevValues => ({ ...prevValues, [key]: value }))
   }
 
-  const handleSubmit = e => {
+  const createUser = e => {
     e.preventDefault()
-    console.log(formData)
+    axios
+      .post(usersUrl, formData, auth)
+      .then(res => {
+        console.log(res.data)
+        const newUser = [...usersList]
+        newUser.push(res.data)
+        setUsersList(newUser)
+      })
   }
 
   return (
     <div className='CreateUser'>
       <div className='CreateUserContainer'>
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={createUser}>
           <label htmlFor='username'>Username</label>
           <input required name='username' type='text' value={username} onChange={handleChange} />
+          <label htmlFor='password'>Password</label>
+          <input required name='password' type='password' value={password} onChange={handleChange} />
           <label htmlFor='firstname'>Firstname</label>
           <input required name='firstname' type='text' value={firstname} onChange={handleChange} />
           <label htmlFor='lastname'>Lastname</label>
           <input required name='lastname' type='text' value={lastname} onChange={handleChange} />
-          <label htmlFor='email'>Email</label>
-          <input required name='email' type='email' value={email} onChange={handleChange} />
+          <label htmlFor='email_address'>Email</label>
+          <input required name='email_address' type='email' value={email_address} onChange={handleChange} />
           <label htmlFor='purpose'>Purpose</label>
           <input required name='purpose' type='text' value={purpose} onChange={handleChange} />
           <input type='submit' value='SAVE' />
